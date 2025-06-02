add601b2d00b6b92dc88e3daca4689b6e5a714dd
Dari-OS <darius.dev@proton.me>
2025-06-02 12:33:33 +0200

    Removed massive amount of code duplication

--- backend/Global/OAuth/API.cs
+++ backend/Global/OAuth/API.cs
@@ -23,75 +23,41 @@ public static class API
     {
         using HttpClient client = new();
 
-        client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue(token.TokenType, token.AccessToken);
 
+        var response = await Fetch(endpoint: "https://discord.com/api/users/@me", transformers: httpClient =>
+        {
+            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue(token.TokenType, token.AccessToken);
+        });
+
+        if (response is null)
+        {
+            Global.Log.Logger.Debug("The user provided a wrong code");
+            // This assumes that every fetch error unrelated to HttpRequestException 
+            // is due to a wrongly provided code.
+            return null;
+        }
 
         try
         {
-            for (int attempt = 0; attempt < 3; attempt++)
+            string content = await response.Content.ReadAsStringAsync();
+            using JsonDocument doc = JsonDocument.Parse(content);
+            var user = new User
             {
-                var response = await client.GetAsync("https://discord.com/api/users/@me");
+                ID = doc.RootElement.GetProperty("id").GetString() ?? throw new NullReferenceException("The returned id is null"),
+                Username = doc.RootElement.GetProperty("username").GetString() ?? throw new NullReferenceException("The returned username is null"),
+                Avatar = doc.RootElement.GetProperty("avatar").GetString() ?? throw new NullReferenceException("The avatar is null"),
+                Premium = (Global.OAuth.User.Nitro)doc.RootElement.GetProperty("premium_type").GetSByte(),
+                GlobalName = doc.RootElement.GetProperty("global_name").GetString() ?? throw new NullReferenceException("The avatar is null"),
+                Mfa = doc.RootElement.GetProperty("mfa_enabled").GetBoolean(),
+            };
 
-                string content = await response.Content.ReadAsStringAsync();
-                if (response.IsSuccessStatusCode)
-                {
-
-
-                    using JsonDocument doc = JsonDocument.Parse(content);
-                    var user = new User
-                    {
-                        ID = doc.RootElement.GetProperty("id").GetString() ?? throw new NullReferenceException("The returned id is null"),
-                        Username = doc.RootElement.GetProperty("username").GetString() ?? throw new NullReferenceException("The returned username is null"),
-                        Avatar = doc.RootElement.GetProperty("avatar").GetString() ?? throw new NullReferenceException("The avatar is null"),
-                        Premium = (Global.OAuth.User.Nitro)doc.RootElement.GetProperty("premium_type").GetSByte(),
-                        GlobalName = doc.RootElement.GetProperty("global_name").GetString() ?? throw new NullReferenceException("The avatar is null"),
-                        Mfa = doc.RootElement.GetProperty("mfa_enabled").GetBoolean(),
-                    };
-
-                    return user;
-                }
-                else if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
-                {
-                    Global.Log.Logger.Debug("The user provided a wrong code");
-                    return null;
-                }
-                else if (response.StatusCode == System.Net.HttpStatusCode.TooManyRequests)
-                {
-                    int retryAfterMs = JsonDocument.Parse(content).RootElement.GetProperty("retry_after").GetInt32();
-
-                    Global.Log.Logger.Warning(
-                        @$"Rate limited. Attempting {3 - attempt} more times.
-                    Waiting {retryAfterMs}ms before retrying...");
-                    await Task.Delay(retryAfterMs);
-                    continue;
-
-                }
-                else
-                {
-                    Global.Log.Logger.Error($"unexpected HTTP Error: {response.StatusCode} - {content}");
-                    return null;
-                }
-            }
+            return user;
         }
-        catch (HttpRequestException ex)
+        catch (NullReferenceException ex)
         {
-            Global.Log.Logger.Error(ex, $"Request failed: {ex.Message}");
+            Global.Log.Logger.Error(ex, $"Could not parse API response to User: {ex.Message}");
             throw;
         }
-        catch (JsonException ex)
-        {
-            Global.Log.Logger.Error(ex, $"JSON parsing failed: {ex.Message}");
-            return null;
-        }
-        catch (Exception ex)
-        {
-            Global.Log.Logger.Error(ex, $"Unexpected error: {ex.Message}");
-            return null;
-        }
-
-
-        Global.Log.Logger.Error("Max retry attempts reached.");
-        return null;
     }
 
     /// <summary>
@@ -156,66 +122,41 @@ public static class API
     /// </exception>
     private static async Task<Token?> FetchToken(FormUrlEncodedContent form, Client client)
     {
-
-
-        using HttpClient httpClient = new();
-        var authToken = Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes($"{client.ID}:{client.Secret}"));
-        httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", authToken);
-
         try
         {
-
-            for (int attempt = 0; attempt < 3; attempt++)
+            var response = await Fetch($"{client.ApiEndpoint}/oauth2/token", form, httpClient =>
             {
-                var response = await httpClient.PostAsync($"{client.ApiEndpoint}/oauth2/token", form);
-                string content = await response.Content.ReadAsStringAsync();
-                if (response.IsSuccessStatusCode)
-                {
+                var authToken = Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes($"{client.ID}:{client.Secret}"));
+                httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", authToken);
+            });
 
-                    using JsonDocument doc = JsonDocument.Parse(content);
-                    Token token = new Token
-                    {
-                        TokenType = doc.RootElement.GetProperty("token_type").GetString() ?? throw new NullReferenceException("The returned token_type is null"),
-                        AccessToken = doc.RootElement.GetProperty("access_token").GetString() ?? throw new NullReferenceException("The returned  is null"),
-                        RefreshToken = doc.RootElement.GetProperty("refresh_token").GetString() ?? throw new NullReferenceException("The returned refresh_token is null"),
-                        ExpiresIn = doc.RootElement.GetProperty("expires_in").GetInt64(),
-                        CreatedAt = DateTime.UtcNow,
-                        Scope = (doc.RootElement.GetProperty("scope").GetString() ?? "").Split(" ").ToHashSet<string>(),
-                    };
-                    return token;
-                }
-                else if (response.StatusCode == System.Net.HttpStatusCode.TooManyRequests)
-                {
-
-                    int retryAfterMs = JsonDocument.Parse(content).RootElement.GetProperty("retry_after").GetInt32();
-                    Global.Log.Logger.Warning(
-                        @$"Rate limited. Attempting {3 - attempt} more times.
-                    Waiting {retryAfterMs}ms before retrying...");
-                    await Task.Delay(retryAfterMs);
-                    continue;
-
-                }
-                else
-                {
-                    Global.Log.Logger.Debug($"Error from Discord API while fetching Token: {content.ToString()}");
-                    return null;
-                }
+            if (response is null)
+            {
+                return null;
             }
+
+            var content = await response.Content.ReadAsStringAsync();
+
+            if (content is not null)
+            {
+
+                using JsonDocument doc = JsonDocument.Parse(content);
+                Token token = new Token
+                {
+                    TokenType = doc.RootElement.GetProperty("token_type").GetString() ?? throw new NullReferenceException("The returned token_type is null"),
+                    AccessToken = doc.RootElement.GetProperty("access_token").GetString() ?? throw new NullReferenceException("The returned access_token is null"),
+                    RefreshToken = doc.RootElement.GetProperty("refresh_token").GetString() ?? throw new NullReferenceException("The returned refresh_token is null"),
+                    ExpiresIn = doc.RootElement.GetProperty("expires_in").GetInt64(),
+                    CreatedAt = DateTime.UtcNow,
+                    Scope = (doc.RootElement.GetProperty("scope").GetString() ?? "").Split(" ").ToHashSet<string>(),
+                };
+                return token;
+            }
+
         }
-        catch (HttpRequestException ex)
+        catch (NullReferenceException ex)
         {
-            Global.Log.Logger.Error(ex, $"Request failed: {ex.Message}");
-            throw;
-        }
-        catch (JsonException ex)
-        {
-            Global.Log.Logger.Error(ex, $"JSON parsing failed: {ex.Message}");
-            return null;
-        }
-        catch (Exception ex)
-        {
-            Global.Log.Logger.Error(ex, $"Unexpected error: {ex.Message}");
-            return null;
+            Global.Log.Logger.Error(ex, $"Could not parse API response to Token: {ex.Message}");
         }
         return null;
     }
@@ -235,24 +176,64 @@ public static class API
     {
 
         using HttpClient httpClient = new();
-        var authToken = Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes($"{client.ID}:{client.Secret}"));
-        httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", authToken);
 
         var form = new FormUrlEncodedContent(new[] {
                 new KeyValuePair<string, string>("token", token.AccessToken),
                 new KeyValuePair<string, string>("token_type_hint", "access_token")
         });
 
+        var response = await Fetch($"{client.ApiEndpoint}/oauth2/token/revoke", form, httpClient =>
+        {
+            var authToken = Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes($"{client.ID}:{client.Secret}"));
+            httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", authToken);
+        });
+
+        if (response is null)
+        {
+            return false;
+        }
+        return true;
+
+    }
+
+    /// <summar>
+    /// Fetches the given endpoint and returns the content.
+    /// 
+    /// </summary>
+    /// <param name="endpoint">The endpoint which gets fetched</param>
+    /// <param name="form">If this parameter is not null the request is a <c>POST</c> request. This is the body of the <c>POST</c> request</param>
+    /// <returns>
+    /// The content as string or null if an error occured
+    /// </returns>
+    /// <exception cref="HttpRequestException">
+    /// Thrown when the HTTP request fails due to network issues or an invalid response from the server.
+    /// </exception>
+    private static async Task<HttpResponseMessage?> Fetch(string endpoint, FormUrlEncodedContent? form = null, params Action<HttpClient>[] transformers)
+    {
+
+        using HttpClient httpClient = new();
         try
         {
-
+            foreach (var transformer in transformers)
+            {
+                transformer(httpClient);
+            }
+            //TODO: Make retry attempts configurable
             for (int attempt = 0; attempt < 3; attempt++)
             {
-                var response = await httpClient.PostAsync($"{client.ApiEndpoint}/oauth2/token/revoke", form);
+                HttpResponseMessage response = null!;
+                if (form is null)
+                {
+                    response = await httpClient.GetAsync(endpoint);
+                }
+                else
+                {
+                    response = await httpClient.PostAsync(endpoint, form);
+                }
                 string content = await response.Content.ReadAsStringAsync();
                 if (response.IsSuccessStatusCode)
                 {
-                    return true;
+                    return response;
                 }
                 else if (response.StatusCode == System.Net.HttpStatusCode.TooManyRequests)
                 {
@@ -267,8 +248,8 @@ public static class API
                 }
                 else
                 {
-                    Global.Log.Logger.Debug($"Error from Discord API while revoking user Token: {content.ToString()}");
-                    return false;
+                    Global.Log.Logger.Debug($"Error while fetching {endpoint}: {content.ToString()}");
+                    return null;
                 }
             }
         }
@@ -280,14 +261,15 @@ public static class API
         catch (JsonException ex)
         {
             Global.Log.Logger.Error(ex, $"JSON parsing failed: {ex.Message}");
-            return false;
+            return null;
         }
         catch (Exception ex)
         {
             Global.Log.Logger.Error(ex, $"Unexpected error: {ex.Message}");
-            return false;
+            return null;
         }
-        return false;
+
+        return null;
     }
 
     /// <summary>
